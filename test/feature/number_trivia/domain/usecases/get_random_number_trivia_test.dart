4bfb42ba55907c788a5c283c399773b24056763d
muhibush <ukiacrew@gmail.com>
2021-11-27 15:41:24 +0700

    BREAKING CHANGE: replace mockito with mocktail

--- test/feature/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/feature/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,32 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tdd/core/usecase/usecase.dart';
 import 'package:tdd/feature/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/feature/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:tdd/feature/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:tdd/feature/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  late GetConcreteNumberTrivia useCase;
+  late GetRandomNumberTrivia useCase;
   late MockNumberTriviaRepository repository;
-  const tNumber = 1;
   var tNumberTrivia = const NumberTrivia(number: 1, text: 'text');
 
   setUpAll(() {
     repository = MockNumberTriviaRepository();
-    useCase = GetConcreteNumberTrivia(repository);
+    useCase = GetRandomNumberTrivia(repository);
   });
 
   test('get trivia number', () async {
-    when(() => repository.getConcreteNumberTrivia(any()))
+    when(() => repository.getRandomNumberTrivia())
         .thenAnswer((_) async => Right(tNumberTrivia));
 
-    final result = await useCase(const Params(number: tNumber));
+    final result = await useCase(NoParams());
 
     expect(result, Right(tNumberTrivia));
-    verify(() => repository.getConcreteNumberTrivia(tNumber)).called(1);
+    verify(() => repository.getRandomNumberTrivia()).called(1);
     verifyNoMoreInteractions(repository);
   });
 }
